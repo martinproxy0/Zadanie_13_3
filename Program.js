bf63ffed07152c05cde32cab9cdea1b8ba5ac703
martinproxy0 <marcinsornek@gmail.com>
2017-12-28 03:26:14 +0100

    Add condition for '/exit' instruction

---
+++ Program.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
